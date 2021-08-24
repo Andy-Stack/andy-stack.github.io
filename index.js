f801ab077e435fdb5cb773aa87801445b362780a
Andy-Stacks <andyatuni@outlook.com>
2021-08-24 19:56:16 +0100

    Added title and tab index attributes. Added on focus styling for accessibility. Added final project description (links still to be added). Minor changes to text content and styling.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+"use strict";
+
+var menu_open = false;
+
+$(document).ready(() => {
+    $(document).keydown((e) => {
+        const element = $(document.activeElement);
+        //open accordian when enter is pressed and accordian selected (accessibility)
+        if (e.key === "Enter" && element.hasClass('accordion')) {
+            element.click();
+        } else if (e.key === "Enter" && element.hasClass('mob-menu')) {
+            nav_open();
+        } else if (e.key === "Escape" && menu_open) {
+            nav_close();
+        }
+    })
+    //set menu_open to false when mobile menu is closed
+    $('#myOverlay').click(() => {
+        menu_open = false;
+    })
+});
+
 function nav_open() {
     $('#mySidebar').show();
     $('#myOverlay').show();
+    $('#homeLink').focus();
+    menu_open = true;
 }
 
 function nav_close() {
     $('#mySidebar').hide();
     $('#myOverlay').hide();
+    $('.mob-menu').focus();
+    menu_open = false;
 }
 
 $(document).on('click', '.ptab', (e) => {
